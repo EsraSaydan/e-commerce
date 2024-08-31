a2d2d10d62433132946933c1a6d0b5e6bc616901
EsraSaydan <esracarturk.77@gmail.com>
2024-08-31 03:53:22 +0300

    T2 tasks are in progress

--- TrendHub/src/App.jsx
+++ TrendHub/src/App.jsx
@@ -1,16 +1,13 @@
 import "./App.css";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+//import { ToastContainer, toast } from "react-toastify";
+//import "react-toastify/dist/ReactToastify.css";
+import Header from "./layout/Header";
 
 function App() {
   return (
     <div>
-      <ToastContainer />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button onClick={() => toast("Hello, World!")}>Show Toast</button>
-      <button className="btn">
-        <i className="fas fa-check-circle"></i> Submit
-      </button>
+      <Header />
     </div>
   );
 }
---
+++ TrendHub/src/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Home/Navbar";
+import Hero from "../components/Home/Hero/Hero";
+
+export default function Header() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
